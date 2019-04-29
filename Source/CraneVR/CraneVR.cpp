1e5880188cec418cdce0ee251ec507c3705c5b99
Jorma <jorma.rebane@gmail.com>
2019-04-29 19:45:19 +0300

    Initial content commit for CraneVR

---
+++ Source/CraneVR/CraneVR.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2019 - Jorma Rebane 3DCrane UE4
+#include "CraneVR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CraneVR, "CraneVR" );
---
+++ Source/CraneVR/CraneVR.h
@@ -0,0 +1,3 @@
+// Copyright (c) 2019 - Jorma Rebane 3DCrane UE4
+#pragma once
+#include "CoreMinimal.h"
